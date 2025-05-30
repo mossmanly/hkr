6e25d9d2b2c7e146cb15691d675e62c1a882034a
moss@humankindholdings.com <moss@humankindholdings.com>
2025-05-30 04:59:32 +0000

    Finalize rent_roll_assumptions model & tests

--- models/rent_roll_assumptions.sql
+++ models/rent_roll_assumptions.sql
@@ -1,16 +1,17 @@
 {{ config(materialized="view") }}
 
-with rent_roll as (            -- 0 spaces before “with”
-  select *                     -- 2 spaces before “select”
-  from {{ ref('rent_roll') }} -- still 2 spaces
-)                              -- 0 spaces before “)”
+with rent_roll as (
+  select * from {{ ref('rent_roll') }}
+)
 
-select                         -- 0 spaces before “select”
-  property_id,                 -- 2 spaces
-  unit_number,                 -- 2 spaces
-  rent_amount,                 -- 2 spaces
-  square_feet,                 -- 2 spaces
-  rent_amount / nullif(        -- 2 spaces
-    square_feet, 0            -- 4 spaces inside nullif
-  ) as rent_per_sqft          -- 2 spaces closing nullif
-from rent_roll                -- 0 spaces
+select
+  company_id,
+  portfolio_id,
+  property_id,
+  acquisition_year,
+  unit_count        as unit_number,
+  avg_rent_per_unit as rent_amount,
+  turnover_rate,
+  turnover_rate_bump,
+  rent_growth_rate
+from rent_roll
