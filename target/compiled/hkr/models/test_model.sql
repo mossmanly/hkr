71341bf69349d62001502544c67e3f79c0742129
Moss <moss@humankindholdings.com>
2025-06-02 19:27:21 -0700

    checkpoint – compiles but IRR empty

---
+++ target/compiled/hkr/models/test_model.sql
@@ -0,0 +1,5 @@
+
+
+select
+  1 as id,
+  'hello dbt' as message
---
+++ target/run/hkr/models/test_model.sql
@@ -0,0 +1,11 @@
+
+  create view "hkh_decision_support_db"."hkr_dbt"."test_model__dbt_tmp"
+    
+    
+  as (
+    
+
+select
+  1 as id,
+  'hello dbt' as message
+  );
